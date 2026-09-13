5bd010a1de333497b08e55b4a4f4f6130a5edbea
ronvotri <lengochung28191@gmail.com>
2026-09-14 00:05:26 +0700

    Gaia Master: add 0.6.11.0 Batch 2 launcher

---
+++ games/GaiaMaster-Kamigami-no-Board-Game/tools/00_BUILD_0.6.11.0_HYBRID_ACCENT_B2.cmd
@@ -0,0 +1,13 @@
+@echo off
+setlocal
+cd /d "%~dp0"
+
+if "%~1"=="" (
+  echo Drag CLEAN Gaia Master BIN onto this CMD.
+  pause
+  exit /b 1
+)
+
+py -3 build_gaia_06110_hybrid_accent_b2.py "%~1"
+echo.
+pause
